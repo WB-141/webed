8432160ae36d982f410fc34b6916bd092230b9e0
Ruben <35366795+WB-141@users.noreply.github.com>
2025-08-01 12:03:44 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,18 +1,36 @@
 function fadeHeader() {
-  const headerBG = document.querySelector("headerBG");
-  const headerdif = document.querySelector("header-different");
+  const headerBG = document.getElementById("headerbg");
+  const headerdif = document.getElementById("headerdif");
+  const navtext = document.querySelectorAll(".nav-text-color");
+  const herocover = document.getElementById("h-cover");
+  const herotext = document.getElementById("herotext")
+
   headerBG.style.opacity = 1;
   headerdif.style.opacity = 0;
+  herocover.style.opacity = 0.6;
+  herotext.style.opacity = 1;
 
   window.addEventListener("scroll", function () {
-    const scrollPos = this.window.scrollY;
+    const scrollPos = window.scrollY;
 
     if (scrollPos > 0) {
       headerBG.style.opacity = 0;
       headerdif.style.opacity = 1;
+      herocover.style.opacity = 0.4;
+      herotext.style.opacity = 0;
+
+      navtext.forEach(function (text) {
+        text.style.color = "#3960b4";
+      });
+
     } else {
       headerBG.style.opacity = 1;
       headerdif.style.opacity = 0;
+      herocover.style.opacity = 0.6;
+      herotext.style.opacity =1;
+      navtext.forEach(function (text) {
+        text.style.color = "#ffffffff";
+      });
     }
   });
 }
